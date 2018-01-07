d0f4c536b68e648d543c088177dc34a9d1e34b24
ROOT\kumaran.kanagaraj <kumaran.kanagaraj@syncfusion.com>
2018-01-07 16:30:09 +0530

    added mcanvas library

--- main.ts
+++ main.ts
@@ -2,6 +2,8 @@ import {Observable} from "rxjs"
 // import {Observable} from "rxjs/Observable";
 // import '.rxjs/add/operator/map';
 // import '.rxjs/add/operator/filter';
+import * as $ from "jquery";
+import MCanvas from 'mcanvas';
 
 // let numbers = [1,2,3,4,5]
 let numbers = []
@@ -40,6 +42,40 @@ class MouseCapture{
     }
 }
 
+class ImageEditor{
+    
+    addTextToImage(){
+        var mc = new MCanvas(1000,1500,'black');
+        mc.background('./sample.jpg',{
+            left:0,
+            top:0,
+            color:'#000000',
+            type:'origin',
+        }).
+            text('<b>Hi dude</b>Samle Okay<s>@Coolbro</s>',{
+            width:'500',
+            align:'left',
+            largeStyle:{
+                color:'red'
+            },
+            normalStyle:{
+                color:'blue'
+            },
+            smallStyle:{
+                color:'yellow'
+            },
+            pos:{
+                x:'center',
+                y:'bottom:200'
+            },
+        }).
+        draw( b64 =>{
+            $('.placeholder').attr('src',b64);
+            // console.log(b64);
+        });
+    }
+}
+
 interface XandYCordoniates{
     x:number;
     y:number;
@@ -60,17 +96,22 @@ let eventCapture = Observable.fromEvent(document,"mousemove")
                               .map((e:MouseEvent) => mouseCapture.getXandYposition(e))
                               .filter(value=>mouseCapture.filterXandYCordoniates(value));
 
-eventCapture.subscribe(observer.next,observer.error,observer.complete);
+// eventCapture.subscribe(observer.next,observer.error,observer.complete);
 
+var imgButton = document.querySelector('.api-img');
+var imgClick = Observable.fromEvent(imgButton, 'click');
+imgClick.subscribe ((e) => {
+    new ImageEditor().addTextToImage();
+  });
 
-source.subscribe(observer.next,observer.error,observer.complete);   // By passing function  Method III
+// source.subscribe(observer.next,observer.error,observer.complete);   // By passing function  Method III
 
-source.subscribe(
-    value => console.log(value),          //By ES6 Method II
-    err => console.error(err),
-    () => console.log("complete")
-);
+// source.subscribe(
+//     value => console.log(value),          //By ES6 Method II
+//     err => console.error(err),
+//     () => console.log("complete")
+// );
 
-source.subscribe(new MyObserver());  // By passing class  Method I
+// source.subscribe(new MyObserver());  // By passing class  Method I
 console.log("Hi there lets go ..");
 
